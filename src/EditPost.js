b347f0c8228576d6aa28873c1c3fe0677ca55967
Peter Horvath <gkipeter@gmail.com>
2021-08-22 16:57:02 +0200

    app refactored

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,58 +1,87 @@
-import { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useEffect, useContext, useState } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
+import { format } from "date-fns";
+import api from "./api/posts";
+import DataContext from "./context/DataContext";
 
+const EditPost = () => {
+	const [editTitle, setEditTitle] = useState("");
+	const [editBody, setEditBody] = useState("");
+	const { posts, setPosts } = useContext(DataContext);
+	const history = useHistory();
+	const { id } = useParams();
+	const post = posts.find((post) => post.id.toString() === id);
 
-const EditPost = ({
-    posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle
-}) => {
-    const { id } = useParams();
-    const post = posts.find(post => (post.id).toString() === id);
+	useEffect(() => {
+		if (post) {
+			setEditTitle(post.title);
+			setEditBody(post.body);
+		}
+	}, [post, setEditTitle, setEditBody]);
 
-    useEffect(() => {
-        if (post) {
-            setEditTitle(post.title);
-            setEditBody(post.body);
-        }
-    }, [post, setEditTitle, setEditBody]);
+	const handleEdit = async (id) => {
+		const datetime = format(new Date(), "MMMM dd, yyyy pp");
+		const updatedPost = { id, title: editTitle, datetime, body: editBody };
 
-    return (
-        <main className="NewPost">
-            {editTitle &&
-                <>
-                    <h2>Edit Post</h2>
-                    <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
-                        <label htmlFor="postTitle">Title:</label>
-                        <input
-                            id="postTitle"
-                            required
-                            value={editTitle}
-                            onChange={(e) => setEditTitle(e.target.value)}
-                        />
+		try {
+			const response = await api.put(`/posts/${id}`, updatedPost);
+			setPosts(
+				posts.map((post) =>
+					post.id === id ? { ...response.data } : post
+				)
+			);
+			setEditTitle("");
+			setEditBody("");
+			history.push("/");
+		} catch (err) {
+			console.log(`Error: ${err.message}`);
+		}
+	};
 
-                        <label htmlFor="postBody">Post:</label>
-                        <textarea
-                            id="postBody"
-                            required
-                            value={editBody}
-                            onChange={(e) => setEditBody(e.target.value)}
-                        >
-                        </textarea>
-                        <button type="submit" onClick={() => handleEdit(post.id)}>Submit</button>
-                    </form>
-                </>
-            }
-            {!editTitle &&
-                <>
-                    <h2>Post not found</h2>
-                    <p>Well, that's disappointing.</p>
-                    <p>
-                        <Link to='/'>Visit Our Homepage</Link>
-                    </p>
-                </>
-            }
+	return (
+		<main className="NewPost">
+			{editTitle && (
+				<>
+					<h2>Edit Post</h2>
+					<form
+						className="newPostForm"
+						onSubmit={(e) => e.preventDefault()}
+					>
+						<label htmlFor="postTitle">Title:</label>
+						<input
+							id="postTitle"
+							required
+							value={editTitle}
+							onChange={(e) => setEditTitle(e.target.value)}
+						/>
 
-        </main>
-    )
-}
+						<label htmlFor="postBody">Post:</label>
+						<textarea
+							id="postBody"
+							required
+							value={editBody}
+							onChange={(e) => setEditBody(e.target.value)}
+						></textarea>
+						<button
+							type="submit"
+							onClick={() => handleEdit(post.id)}
+						>
+							Submit
+						</button>
+					</form>
+				</>
+			)}
+			{!editTitle && (
+				<>
+					<h2>Post not found</h2>
+					<p>Well, that's disappointing.</p>
+					<p>
+						<Link to="/">Visit Our Homepage</Link>
+					</p>
+				</>
+			)}
+		</main>
+	);
+};
 
-export default EditPost
+export default EditPost;
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,35 @@
-import Feed from './Feed';
+import Feed from "./Feed";
+import { useContext } from "react";
+import DataContext from "./context/DataContext";
 
-const Home = ({ posts, fetchError, isLoading }) => {
-    return (
-        <main className="Home">
-            {/* {posts.length ? (
+const Home = () => {
+    const { searchResults, fetchError, isLoading } = useContext(DataContext);
+
+	return (
+
+		<main className="Home">
+			{/* {posts.length ? (
                 <Feed posts={posts} />
             ) : (
                 <p style={{ marginTop: "2rem"}}>
                     No posts to display...
                 </p>
             )} */}
-            {isLoading && <p className="statusMsg">Loading posts...</p>}
-            {!isLoading && fetchError && <p className="statusMsg" style={{color: "red"}}>{fetchError}</p>}
-            {!isLoading && !fetchError && (posts.length ? <Feed posts={posts} /> : <p className="statusMsg">No posts to display...</p>)}
-        </main>
-    )
-}
+			{isLoading && <p className="statusMsg">Loading posts...</p>}
+			{!isLoading && fetchError && (
+				<p className="statusMsg" style={{ color: "red" }}>
+					{fetchError}
+				</p>
+			)}
+			{!isLoading &&
+				!fetchError &&
+				(searchResults.length ? (
+					<Feed posts={searchResults} />
+				) : (
+					<p className="statusMsg">No posts to display...</p>
+				))}
+		</main>
+	);
+};
 
-export default Home
+export default Home;
